d56464a06f85a6ee18fc7739eb1770db0146505c
Henning Redestig <henning.red@gmail.com>
2016-07-27 13:04:52 +0200

    skip cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 set -e
 echo -e " ... running twine to deploy ..."
+pip install twine
 twine upload ${TRAVIS_BUILD_DIR}/wheelhouse/*.whl --skip-existing -u henred -p $PYPIPWD
 
 # twine upload --skip-existing --username hredestig --password "${PYPIPWD}" dist/*
