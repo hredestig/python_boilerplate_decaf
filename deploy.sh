cdf5eed405df9754debf3a914c5174a79a282041
Henning Redestig <henning.red@gmail.com>
2016-07-27 09:39:22 +0200

    fixes..
    squash! fixes..

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-pip install twine
-twine upload --skip-existing --username hredestig --password "${PYPIPWD}" dist/*
-twine upload --skip-existing --username hredestig --password "${PYPIPWD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
