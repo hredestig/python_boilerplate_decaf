ddce3294ac82157a6395ca89aaf6b3f4778ab58b
Henning Redestig <henning.red@gmail.com>
2016-07-27 12:55:30 +0200

    use deploy instead

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 set -e
 echo -e " ... running twine to deploy ..."
+twine upload ${TRAVIS_BUILD_DIR}/wheelhouse/*.whl --skip-existing -u henred -p $PYPIPWD
+
 # twine upload --skip-existing --username hredestig --password "${PYPIPWD}" dist/*
 # twine upload --skip-existing --username hredestig --password "${PYPIPWD}" ${TRAVIS_BUILD_DIR}/wheelhouse/*
 exit 0;
